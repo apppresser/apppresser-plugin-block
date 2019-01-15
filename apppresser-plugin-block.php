1ef554d9845d9fdf064bde1c64995fd5d6b07e39
Scott Bolinger <designbyscott@gmail.com>
2019-01-15 11:24:18 -0800

    Add ability to deactivate plugins for API calls
    This will help debug API calls

--- apppresser-plugin-block.php
+++ apppresser-plugin-block.php
@@ -3,8 +3,8 @@
 Plugin Name: AppPresser Plugin Block
 Description: Blocks plugins from loading in AppPreser app.
 Author: AppPresser Team
-Version: 2.0
-Author URI: https://apppresser.com
+Version: 1.0
+Author URI: http://apppresser.com
 */
 
 class AppPresserPluginBlock {
@@ -13,7 +13,7 @@ class AppPresserPluginBlock {
 	public static $instance    = null;
 	public static $this_plugin = null;
 	const PLUGIN               = 'AppPresser Plugin Block';
-	const VERSION              = '2.0';
+	const VERSION              = '1.0';
 	public static $dir_path;
 	public static $dir_url;
 	public static $is_apppv;
@@ -52,7 +52,6 @@ class AppPresserPluginBlock {
 		}
 
 		add_filter( 'option_active_plugins', array( $this, 'appp_filter_plugins' ), 5 );
-		add_filter( 'site_option_active_sitewide_plugins', array( $this, 'appp_filter_plugins' ) );
 		
 	}
 	
@@ -60,22 +59,36 @@ class AppPresserPluginBlock {
 	// remove some plugins
 	function appp_filter_plugins( $active = array() ) {
 
-		if( !is_admin() && self::read_app_version() ) {
+		if( is_admin() ) {
+			return $active;
+		}
+
+		// handle API plugin blocking
+		if( get_option( 'apppresser-plugin-block-api' ) === '1' && $_SERVER['REQUEST_URI'] && strpos( $_SERVER['REQUEST_URI'], 'wp-json' ) != false ) {
+			$exclude = apply_filters( 'appp_exclude_plugins', $active );
+
+			$exclude = maybe_unserialize( $exclude );
+	
+			foreach ( $exclude as $plugin ) {
+				$key = array_search( $plugin, $active );
+				if ( false !== $key ) {
+					unset( $active[ $key ] );
+				}
+			}
+
+			return $active;
+		}
+
+		if( self::read_app_version() && ( !function_exists('wp_is_mobile') || function_exists('wp_is_mobile') && wp_is_mobile() ) ) {
 
 			$exclude = apply_filters( 'appp_exclude_plugins', $active );
 
 			$exclude = maybe_unserialize( $exclude );
 	
 			foreach ( $exclude as $plugin ) {
-
-				if( current_filter() == 'option_active_plugins' ) {
-					$key = array_search( $plugin, $active );
-				} else {
-					$key = !empty( $active[$plugin] ) ? $plugin : false;
-				}
-
-				if( false !== $key ) {
-					unset( $active[$key] );
+				$key = array_search( $plugin, $active );
+				if ( false !== $key ) {
+					unset( $active[ $key ] );
 				}
 			}
 		}
@@ -87,9 +100,7 @@ class AppPresserPluginBlock {
 		if ( self::$is_apppv !== null )
 			return self::$is_apppv;
 
-		if( isset( $_SERVER['REQUEST_URI'] ) && strpos( $_SERVER['REQUEST_URI'], '/wp-json/ap-bp/' ) !== false ) {
-			self::$is_apppv = 3;
-		} else if( isset( $_GET['appp'] ) && $_GET['appp'] == 3 || isset( $_COOKIE['AppPresser_Appp3'] ) && $_COOKIE['AppPresser_Appp3'] === 'true' ) {
+		if( isset( $_GET['appp'] ) && $_GET['appp'] == 3 || isset( $_COOKIE['AppPresser_Appp3'] ) && $_COOKIE['AppPresser_Appp3'] === 'true' ) {
 			self::$is_apppv = 3;
 		} else if( isset( $_GET['appp'] ) && $_GET['appp'] == 2 || isset( $_COOKIE['AppPresser_Appp2'] ) && $_COOKIE['AppPresser_Appp2'] === 'true' ) {
 			self::$is_apppv = 2;
@@ -143,12 +154,21 @@ function apb_admin_init() {
     register_setting( 'apppresser-plugin-block-group', 'apppresser-plugin-block', 'apb_validate_input' );
     add_settings_section( 'section-one', 'Choose Plugins', 'section_one_callback', 'apppresser-plugin-block' );
     add_settings_field( 'field-one', 'Plugins', 'field_one_callback', 'apppresser-plugin-block', 'section-one' );
+
+    register_setting( 'apppresser-plugin-block-group', 'apppresser-plugin-block-api', 'apb_validate_input' );
+    add_settings_field( 'field-two', 'Deactivate for WP-API Calls', 'field_two_callback', 'apppresser-plugin-block', 'section-one' );
 }
 add_action( 'admin_init', 'apb_admin_init' );
 
 
 function section_one_callback() {
     echo 'Checked plugins will be deactived on mobile.';
+}
+
+function field_two_callback() {
+	$setting = maybe_unserialize( get_option( 'apppresser-plugin-block-api' ) );
+
+	echo '<p><input type="checkbox" ' . ( $setting ? "checked" : "" ) . ' name="apppresser-plugin-block-api" value="1" />Deactive for WP-API (check if you have issues with API based integrations like AppCommunity or AppCommerce)</p>';
 }
 
 function field_one_callback() {
@@ -171,19 +191,16 @@ function field_one_callback() {
     $plugins = get_plugins();
     
     $keep = array(
-		'apppresser-plugin-block/apppresser-plugin-block.php' => '',
-		'apppresser/apppresser.php' => '',
-		'appgeo/apppresser-geolocation.php' => '',
-		'appbuddy/appbuddy.php' => '',
-		'appcamera/appp-camera.php' => '',
-		'apppush/appp-push.php' => '',
-		'appshare/appshare.php' => '',
-		'appswiper/apppresser-swipers.php' => '',
-		'appfbconnect/appfbconnect.php' => '',
-		'appcommunity/appcommunity.php' => '',
-		'applms/applms.php' => '',
-		'appwoo/apppresser-woocommerce.php' => ''
-	);
+    	'apppresser-plugin-block/apppresser-plugin-block.php' => '',
+    	'apppresser/apppresser.php' => '',
+    	'appgeo/apppresser-geolocation.php' => '',
+    	'appbuddy/appbuddy.php' => '',
+    	'appcamera/appp-camera.php' => '',
+    	'apppush/appp-push.php' => '',
+    	'appshare/appshare.php' => '',
+    	'appswiper/apppresser-swipers.php' => '',
+    	'appwoo/apppresser-woocommerce.php' => ''
+    );
     
     $plugins = array_diff_key($plugins, $keep);        
     $array_keys = array_keys( $plugins );
